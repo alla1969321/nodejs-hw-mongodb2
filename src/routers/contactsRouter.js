25bc15fef8402519722a44439148862c07d2b1c6
alla1969321 <129950799+alla1969321@users.noreply.github.com>
2025-01-20 11:43:04 +0200

    gg

---
+++ src/routers/contactsRouter.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import contactsControllers from '../controllers/contactsControllers.js';
+
+const contactsRouter = express.Router()
+
+contactsRouter.get('/', contactsControllers.getAllController);
+contactsRouter.get('/:contactId', contactsControllers.getByIdController)
+
+export default contactsRouter
