2d91c8727820b7a0187b91c8838cab995a93f44d
alla1969321 <129950799+alla1969321@users.noreply.github.com>
2025-01-20 18:25:56 +0200

    uuu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // src/index.js
-// src/index.js
 
 import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { startServer } from './server.js';
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
@@ -13,6 +17,29 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    message: 'Not found',
+  });
 });
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
+
+app.use(cors());
+
