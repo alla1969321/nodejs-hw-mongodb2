b5a9a4f78ed3b662d648a06dca9d29cad0bd4fc0
alla1969321 <129950799+alla1969321@users.noreply.github.com>
2025-01-23 09:45:14 +0200

    ooo

--- src/index.js
+++ src/index.js
@@ -1,45 +1,10 @@
-// src/index.js
+import { initMongoConnection } from "./db/initMongoConnection.js";
 
-import express from 'express';
-import pino from 'pino-http';
-import cors from 'cors';
-import { startServer } from './server.js';
+import { setupServer } from "./server.js";
 
-const PORT = 3000;
+const bootstrap = async () => {
+    await initMongoConnection();
+    setupServer();
+};
 
-const app = express();
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world!',
-  });
-});
-
-app.use('*', (req, res, next) => {
-  res.status(404).json({
-    message: 'Not found',
-  });
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
-
-app.use(cors());
-
+bootstrap();
