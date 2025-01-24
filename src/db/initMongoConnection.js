0549dded0363a1dd87f85af090219b2b4ca4aad7
alla1969321 <129950799+alla1969321@users.noreply.github.com>
2025-01-24 19:45:56 +0200

    ggg

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -10,7 +10,7 @@ export const initMongoConnection = async () => {
     const db = env('MONGODB_DB');
 
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+      `mongodb+srv://belka3:uTkICqbQY4MPazP1@cluster0.nnm79.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     );
     console.log('Mongo connection successfully established!');
 
